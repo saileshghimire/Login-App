2f8ab7e77e56585711cd0cfebf3fc4510f6c11ca
saileshghimire <saileshghimire1@gmail.com>
2024-08-29 21:27:00 +0545

    recovery-componenet

--- frontend/src/components/Recovery.tsx
+++ frontend/src/components/Recovery.tsx
@@ -1,10 +1,43 @@
 
+import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+
 
 export const Recovery = () => {
+
+
+
     return (
+        <div className="container mx-auto">
+
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
+
+      <div className='flex justify-center items-center h-screen'>
         <div>
-            Recovery
+        <div className={styles.glass}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className='text-5xl font-bold'>Recovery</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+              Enter OTP to recover password. 
+            </span>
+          </div>
+          
+          <form className='py-1' onSubmit={()=>{}} >
+              <div className="textbox flex flex-col items-center gap-6">
+                  <input className={styles.textbox} type="text" placeholder='OPT' />
+                  <button className={styles.btn} type='submit'>Let's Go</button>
+              </div>
+
+              <div className="text-center py-4">
+                <span className='text-gray-500'>Cant't get OPT? <button className='text-red-500'>Resend</button></span>
+              </div>
+
+          </form>
+         
         </div>
+        </div>
+      </div>
+    </div>
     );
 };
-
